8f687016c15d0a907d6e4465b684e31bd056a5e2
MatiasLopez2 <matiaslopez0022@gmail.com>
2026-06-09 21:42:32 -0300

    Fix brand logos responsive layout with CSS Grid

--- src/components/Brands/Brands.jsx
+++ src/components/Brands/Brands.jsx
@@ -6,25 +6,43 @@ import makita from "/img/makita.png"
 import milwaukee from "/img/milwaukee.png"
 import skil from "/img/skil.png"
 import { Link } from "react-router-dom";
+import './Brands.css';
 
 function Brands(){
+    const brands = [
+      { name: 'Stanley', img: stanley, link: '/brand/stanley' },
+      { name: 'Dewalt', img: dewalt, link: '/brand/dewalt' },
+      { name: 'Black+Decker', img: black, link: '/brand/black+decker' },
+      { name: 'Bosch', img: bosch, link: '/brand/bosch' },
+      { name: 'Makita', img: makita, link: '/brand/makita' },
+      { name: 'Milwaukee', img: milwaukee, link: '/brand/milwaukee' },
+      { name: 'Skil', img: skil, link: '/brand/skil' },
+    ];
+
     return(
         <>
     <div className="max-w-5xl mx-auto py-8 px-4">
       <h3 className='section-title'>ENCONTRÁ TU MARCA</h3>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-6 justify-items-center">
-        
-            <picture>
-              <Link to="/brand/stanley"><img src={stanley} style={{ width: '150px' }}  className="h-12 object-contain" loading="lazy" /></Link>
-              <Link to="/brand/dewalt"><img src={dewalt} style={{ width: '150px' }}  className="h-12 object-contain" loading="lazy" /></Link>
-              <Link to="/brand/black+decker"><img src={black} style={{ width: '150px' }}  className="h-12 object-contain" loading="lazy" /></Link>
-              <Link to="/brand/bosch"><img src={bosch} style={{ width: '150px' }}  className="h-12 object-contain" loading="lazy" /></Link>
-              <Link to="/brand/makita"><img src={makita} style={{ width: '150px' }}  className="h-12 object-contain" loading="lazy" /></Link>
-              <Link to="/brand/milwaukee"><img src={milwaukee} style={{ width: '150px' }}  className="h-12 object-contain" loading="lazy" /></Link>
-              <Link to="/brand/skil"><img src={skil} style={{ width: '150px' }}  className="h-12 object-contain" loading="lazy" /></Link>
-              <Link to="/brand/skil2"><img src={skil} style={{ width: '150px' }}  className="h-12 object-contain" loading="lazy" /></Link>
-            </picture>
-       
+      <div className="brands-grid">
+        {brands.map((brand, index) => (
+          <Link 
+            key={index} 
+            to={brand.link}
+            className="brand-item"
+          >
+            <img 
+              src={brand.img} 
+              alt={brand.name}
+              style={{ 
+                width: '100%',
+                maxWidth: '120px',
+                height: 'auto',
+                objectFit: 'contain'
+              }} 
+              loading="lazy" 
+            />
+          </Link>
+        ))}
       </div>
     </div>
         </>
